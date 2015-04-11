29720fe758957aac1e692f1eb3f88096bf03543e
Greg <gregory.desmaziers@backelite.com>
2015-04-11 20:15:53 +0200

    Add propoerty attibutes

--- js/directives/navbar/navbar.js
+++ js/directives/navbar/navbar.js
@@ -1,41 +1,58 @@
 'use strict';
 
 angular.module('bk.angular.ui.navbar', [])
-    .directive("bkNavBar", function () {
+    .directive('bkNavBar', function () {
         return {
+            restrict: 'E',
             replace: true,
             scope: {
-                headerBrandName: "@",
-                headerLogoClassName: "@",
-                headerToggleLabel: "@",
-                leftItems: "=",
-                rightItems: "=",
-                labelProperty: "@",
-                searchFieldEnabled: "@",
-                searchFieldOnLeft: "@",
-                searchFieldPlaceholder: "@",
+                headerBrandName: '@',
+                headerLogoClassName: '@',
+                headerToggleLabel: '@',
+                leftItems: '=',
+                rightItems: '=',
+                labelProperty: '@',
+                iconProperty: '@',
+                urlProperty: '@',
+                childsProperty: '@',
+                searchFieldEnabled: '@',
+                searchFieldOnLeft: '@',
+                searchFieldPlaceholder: '@',
                 searchFieldSubmitFn: '&'
             },
-            templateUrl: "bk.angular.ui.navbar.html"
+            link: function (scope, element, attrs) {
+                if (angular.isDefined(scope.childsProperty)) {
+                    for (var item in scope.leftItems) {
+                        item.childs = item[scope.childsProperty];
+                    }
+                    for (var item in scope.rightItems) {
+                        item.childs = item[scope.childsProperty];
+                    }
+                }
+            },
+            templateUrl: 'bk.angular.ui.navbar.html'
         }
-    }).directive("bkNavBarHeader", function () {
+    }).
+    directive('bkNavBarHeader', function () {
         return {
+            restrict: 'E',
             replace: true,
             scope: {
-                navCollapsed: "=",
-                brandName: "=",
-                logoClassName: "=",
-                toogleLabel: "="
+                navCollapsed: '=',
+                brandName: '=',
+                logoClassName: '=',
+                toogleLabel: '='
             },
-            templateUrl: "bk.angular.ui.navbar.header.html"
+            templateUrl: 'bk.angular.ui.navbar.header.html'
         }
-    }).directive("bkNavBarSearchField", function () {
+    }).directive('bkNavBarSearchField', function () {
         return {
+            restrict: 'E',
             replace: true,
             scope: {
                 placeholder: '=',
                 submitFn: '&'
             },
-            templateUrl: "bk.angular.ui.navbar.searchfield.html"
+            templateUrl: 'bk.angular.ui.navbar.searchfield.html'
         }
     });
