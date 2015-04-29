5ac6db56d6c38f217b793e589624e947a2e5cbd7
Greg <gregory.desmaziers@backelite.com>
2015-04-29 19:28:56 +0200

    Add timestampToDates filter

---
+++ js/filters/timestampToDates/timestampToDates.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('bk.angular.filters.timestampToDates', [])
+    .filter('bkTimestampToDates', function () {
+        return function (data, key) {
+            if (angular.isUndefined(data) || !angular.isArray(data)) {
+                return [];
+            }
+            var dates = [];
+            angular.forEach(data, function (element) {
+                if(angular.isDefined(key) && angular.isString(key)) {
+                    if(angular.isDefined(element[key]) && angular.isNumber(element[key])) {
+                        dates.push(new Date(element[key]));
+                    }
+                }
+                else {
+                    if (angular.isDefined(element) && angular.isNumber(element)) {
+                        dates.push(new Date(element);
+                    }
+                }
+            });
+            return dates;
+        };
+    });
