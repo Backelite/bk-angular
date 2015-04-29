507be6313096e2e0530a4405af20ead0f042c0fd
Greg <gregory.desmaziers@backelite.com>
2015-04-29 20:00:04 +0200

    Fix timestampToDates filter issue

--- js/filters/timestampToDates/timestampToDates.js
+++ js/filters/timestampToDates/timestampToDates.js
@@ -4,21 +4,21 @@ angular.module('bk.angular.filters.timestampToDates', [])
     .filter('bkTimestampToDates', function () {
         return function (data, key) {
             if (angular.isUndefined(data) || !angular.isArray(data)) {
-                return [];
+                return data;
             }
             var dates = [];
             angular.forEach(data, function (element) {
                 if(angular.isDefined(key) && angular.isString(key)) {
                     if(angular.isDefined(element[key]) && angular.isNumber(element[key])) {
-                        dates.push(new Date(element[key]));
+                        element[key] = new Date(element[key]);
                     }
                 }
                 else {
                     if (angular.isDefined(element) && angular.isNumber(element)) {
-                        dates.push(new Date(element));
+                        element = new Date(element);
                     }
                 }
             });
-            return dates;
+            return data;
         };
     });
