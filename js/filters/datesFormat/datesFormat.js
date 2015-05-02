29c670133fc22769513ccacd169b6d0822ffc653
Greg <gregory.desmaziers@backelite.com>
2015-05-02 19:42:31 +0200

    Add bkDatesFormat filter

--- js/filters/datesFormat/datesFormat.js
+++ js/filters/datesFormat/datesFormat.js
@@ -1,20 +1,20 @@
 'use strict';
 
-angular.module('bk.angular.filters.timestampToDates', [])
-    .filter('bkTimestampToDates', function () {
-        return function (data, key) {
-            if (angular.isUndefined(data) || !angular.isArray(data)) {
+angular.module('bk.angular.filters.datesFormat', [])
+    .filter('bkDatesFormat', function ($filter) {
+        return function (data, format, key) {
+            if (angular.isUndefined(data) || !angular.isArray(data) || !angular.isString(format)) {
                 return data;
             }
             angular.forEach(data, function (element) {
                 if(angular.isDefined(key) && angular.isString(key)) {
                     if(angular.isDefined(element[key]) && angular.isNumber(element[key])) {
-                        element[key] = new Date(element[key]);
+                        element[key] = $format('date')(element[key], format);
                     }
                 }
                 else {
                     if (angular.isDefined(element) && angular.isNumber(element)) {
-                        element = new Date(element);
+                        element = $format('date')(element, format);
                     }
                 }
             });
