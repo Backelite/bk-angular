5158ea09ab9039e81acb3a45cc1dc8916d1c49a9
Grégory DESMAZIERS <Greg@MacBook-Pro-de-Gregory.local>
2015-12-05 20:40:44 +0100

    Add objectEquals filter

---
+++ js/filters/objectEquals/objectEquals.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('bk.angular.filters.objectEquals', [])
+    .filter('bkObjectEquals', function ($filter) {
+        return function (object, otherObject, identifierKey) {
+            if (!angular.isObject(object) || !angular.isObject(otherObject)) {
+                return false;
+            }
+            if (angular.isDefined(identifierKey) && angular.isDefined(object[identifierKey]) && angular.isDefined(otherObject[identifierKey])) {
+                return object[identifierKey]===otherObject[identifierKey];
+            }
+            if (angular.isUndefined(identifierKey) && angular.isDefined(object['id']) && angular.isDefined(otherObject['id'])) {
+                return object['id']===otherObject['id'];
+            }
+            return false;
+        };
+    });
